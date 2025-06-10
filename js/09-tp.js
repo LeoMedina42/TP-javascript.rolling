fb30341cd5fdf705b4f8dd61a453bd8460b0fecd
Leo Medina <medinajoseleonardo4@gmail.com>
2025-06-10 17:41:57 -0300

    se hizo ejercicio 9

---
+++ js/09-tp.js
@@ -0,0 +1,16 @@
+// Pedir una frase al usuario
+let frase = prompt("Ingresa una frase:");
+let vocales = ""; // variable vacia, para guardar las vocales encontradas
+
+// para hacer el recorrido de cada letra
+for (let i = 0; i < frase.length; i++) {
+  let letra = frase.charAt(i).toLowerCase(); // pasarlo a miniscula(.tolowercase())
+
+  // comprobar si es vocal
+  if (letra === "a" || letra === "e" || letra === "i" || letra === "o" || letra === "u") {
+    vocales += letra + " ";
+  }
+} //Si es "a" O "e" O "i" O "o" O "u", entonces se cumple el if.
+
+// Mostrar las vocales encontradas
+alert("Vocales encontradas: " + vocales);
