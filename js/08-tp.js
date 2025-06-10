23273a0d96ea5a511c1c6a1930c960f2ec052269
Leo Medina <medinajoseleonardo4@gmail.com>
2025-06-10 16:44:35 -0300

    se creo ejercicio 8

---
+++ js/08-tp.js
@@ -0,0 +1,9 @@
+// Pedir un numero al usuario
+let numero = parseInt(prompt("Ingresa un numero:"));
+
+// comprobar si es divisible por 2
+if (numero % 2 === 0) {
+  alert("El numero es divisible por 2.");
+} else {
+  alert("El numero NO es divisible por 2.");
+}
