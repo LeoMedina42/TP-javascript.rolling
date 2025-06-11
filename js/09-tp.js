e3c77c86592e388415bde1e4dc572f891c8e14ff
Leo Medina <medinajoseleonardo4@gmail.com>
2025-06-11 17:38:40 -0300

    ejercicio nueve con condicion

--- js/09-tp.js
+++ js/09-tp.js
@@ -1,3 +1,5 @@
+/*
+//con bucle for
 // Pedir una frase al usuario
 let frase = prompt("Ingresa una frase:").toLocaleLowerCase();
 let vocales = ""; // variable vacia, para guardar las vocales encontradas
@@ -14,3 +16,51 @@ for (let i = 0; i < frase.length; i++) {
 
 // Mostrar las vocales encontradas
 alert("Vocales encontradas: " + vocales);
+*/
+
+//con condicion 👇
+
+const frase = prompt("ingresa una frase de hasta 4 caracteres").toLowerCase();
+console.log(frase.toLowerCase());
+console.log(frase.toUpperCase());
+console.log(frase.length);
+console.log(frase.substring(0, 3));
+console.log(frase.charAt(0));
+console.log(frase.charAt(5));
+
+if (
+  frase.charAt(0) === "a" ||
+  frase.charAt(0) === "e" ||
+  frase.charAt(0) === "i" ||
+  frase.charAt(0) === "o" ||
+  frase.charAt(0) === "u"
+) {
+  document.writeln(frase.charAt(0));
+}
+if (
+  frase.charAt(1) === "a" ||
+  frase.charAt(1) === "e" ||
+  frase.charAt(1) === "i" ||
+  frase.charAt(1) === "o" ||
+  frase.charAt(1) === "u"
+) {
+  document.writeln(frase.charAt(1));
+}
+if (
+  frase.charAt(2) === "a" ||
+  frase.charAt(2) === "e" ||
+  frase.charAt(2) === "i" ||
+  frase.charAt(2) === "o" ||
+  frase.charAt(2) === "u"
+) {
+  document.writeln(frase.charAt(2));
+}
+if (
+  frase.charAt(3) === "a" ||
+  frase.charAt(3) === "e" ||
+  frase.charAt(3) === "i" ||
+  frase.charAt(3) === "o" ||
+  frase.charAt(3) === "u"
+) {
+  document.writeln(frase.charAt(3));
+}
