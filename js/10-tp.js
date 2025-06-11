a2968b517789e716617ab29edb618641a31c8987
Leo Medina <medinajoseleonardo4@gmail.com>
2025-06-11 20:44:56 -0300

    se hizo ejercicio 10

---
+++ js/10-tp.js
@@ -0,0 +1,13 @@
+let numero = parseInt(prompt("Ingrese un número:"));
+
+if (numero % 2 === 0) {
+  alert("El " + numero + " es divisible por 2.");
+} else if (numero % 3 === 0) {
+  alert("El " + numero + " es divisible por 3.");
+} else if (numero % 5 === 0) {
+  alert("El " + numero + " es divisible por 5.");
+} else if (numero % 7 === 0) {
+  alert("El " + numero + " es divisible por 7.");
+} else {
+  alert("El " + numero + " no es divisible por 2, 3, 5 ni 7.");
+}
